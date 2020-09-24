dbaddc42ac94096d3c6f55680859e08f6346d340
leonleles <leoleles9159@hotmail.com>
2020-09-23 21:00:51 -0300

    Desafio concluido sem testes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,73 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, techs } = request.body;
+  const url = "https://github.com/rocketseat-education/bootcamp-gostack-desafios/";
+  const id = uuid();
+
+  const repository = {
+    id,
+    title,
+    techs,
+    url,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, techs, url } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id = id);
+
+  if (repositoryIndex < 0) return response.status(400).json({ message: "No repository" });
+
+  const repository = {
+    id,
+    title,
+    techs,
+    url,
+    likes: repositories[repositoryIndex].likes
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.status(201).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id = id);
+
+  if (repositoryIndex < 0) return response.status(400).json({ message: "No repository" });
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id = id);
+
+  if (repositoryIndex < 0) return response.status(400).json({ message: "No repository" });
+
+  const { likes } = repositories[repositoryIndex];
+
+  repositories[repositoryIndex].likes = likes + 1;
+
+  return response.status(204).send();
 });
 
 module.exports = app;
